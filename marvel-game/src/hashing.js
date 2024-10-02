f1174c5629fee1ada1a8e53de8f6623659fed8b0
Makovei Miron <miron.makovei@gmail.com>
2024-10-02 20:41:48 +0200

    asd

--- marvel-game/src/hashing.js
+++ marvel-game/src/hashing.js
@@ -6,5 +6,7 @@ export default async function hashing(timestamp){
 
     const hash = await CryptoJS.MD5(`${timestamp}${PRIVATE_KEY}${PUBLIC_KEY}`).toString();
     
+    console.log(hash)
+
     return hash
 }
