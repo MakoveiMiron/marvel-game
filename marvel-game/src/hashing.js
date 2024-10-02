7f4962010c71b10e7cb96d6c99949979d401f142
Makovei Miron <miron.makovei@gmail.com>
2024-10-02 20:38:58 +0200

    hashing fixed

--- marvel-game/src/hashing.js
+++ marvel-game/src/hashing.js
@@ -4,7 +4,7 @@ import { PUBLIC_KEY, PRIVATE_KEY } from './constans'
 
 export default async function hashing(timestamp){
 
-    const hash = await CryptoJS.MD5(`${timestamp}${PUBLIC_KEY}${PRIVATE_KEY}`).toString();
+    const hash = await CryptoJS.MD5(`${timestamp}${PRIVATE_KEY}${PUBLIC_KEY}`).toString();
     
     return hash
 }
