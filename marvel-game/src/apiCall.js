5429597c73558f8c8ee2f5becca8eee4f934d32b
Makovei Miron <miron.makovei@gmail.com>
2024-10-03 00:57:57 +0200

    asd

--- marvel-game/src/apiCall.js
+++ marvel-game/src/apiCall.js
@@ -3,10 +3,12 @@ import { PUBLIC_KEY } from './constans';
 
 export default async function apiCall() {
 
+    const timestamp = Date.now()
+
     const hash = await hashing('1');
     console.log("Hash:", hash);
 
-    const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${PUBLIC_KEY}&hash=${hash}`, {
+    const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=${timestamp}&apikey=${PUBLIC_KEY}&hash=${hash}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
