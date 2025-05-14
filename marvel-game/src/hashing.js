4a6d59faee8fa452d3da5d0b6da49c15139b8033
Makovei Miron <miron.makovei@gmail.com>
2025-05-14 19:02:51 +0200

    asd

--- marvel-game/src/hashing.js
+++ marvel-game/src/hashing.js
@@ -4,9 +4,9 @@ import { PUBLIC_KEY, PRIVATE_KEY } from './constans'
 
 export default function hashing(timestamp){
 
-    console.log(`${timestamp}d5c5f7df458e2e0d3207ec1c9f42acce2739bb2fccbad2fc930ed4575d1bb5e4ba6026cc`)
+    console.log(`${timestamp}${PRIVATE_KEY}${PUBLIC_KEY}`)
 
-    const stringToHash = `${timestamp}d5c5f7df458e2e0d3207ec1c9f42acce2739bb2fccbad2fc930ed4575d1bb5e4ba6026cc`; 
+    const stringToHash = `${timestamp}${PRIVATE_KEY}${PUBLIC_KEY}`; 
 
     
     const hash = CryptoJS.MD5(stringToHash).toString();
