63e8cdbf64b05920aa6258c01c931d4087aef69b
MakoveiMiron <113377612+MakoveiMiron@users.noreply.github.com>
2025-05-14 12:50:43 +0000

    asd

--- marvel-game/src/hashing.js
+++ marvel-game/src/hashing.js
@@ -6,6 +6,7 @@ export default function hashing(timestamp){
 
     const stringToHash = `${timestamp}d5c5f7df458e2e0d3207ec1c9f42acce2739bb2fccbad2fc930ed4575d1bb5e4ba6026cc`; 
 
+    
     const hash = CryptoJS.MD5(stringToHash).toString();
 
     return hash
