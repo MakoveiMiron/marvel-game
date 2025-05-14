2051a2f5489fecf582ec337dbe937e29d9305517
MakoveiMiron <113377612+MakoveiMiron@users.noreply.github.com>
2025-05-14 14:42:48 +0200

    Update hashing.js

--- marvel-game/src/hashing.js
+++ marvel-game/src/hashing.js
@@ -4,7 +4,7 @@ import { PUBLIC_KEY, PRIVATE_KEY } from './constans'
 
 export default function hashing(timestamp){
 
-    const stringToHash = `${timestamp}${PRIVATE_KEY}${PUBLIC_KEY}`; 
+    const stringToHash = `${timestamp}d5c5f7df458e2e0d3207ec1c9f42acce2739bb2fccbad2fc930ed4575d1bb5e4ba6026cc`; 
 
     const hash = CryptoJS.MD5(stringToHash).toString();
 
