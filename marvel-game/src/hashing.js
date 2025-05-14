836065f7f4608f34671f8e2c77a4efb10e69fb03
Makovei Miron <miron.makovei@gmail.com>
2025-05-14 18:49:52 +0200

    asd

--- marvel-game/src/hashing.js
+++ marvel-game/src/hashing.js
@@ -4,6 +4,8 @@ import { PUBLIC_KEY, PRIVATE_KEY } from './constans'
 
 export default function hashing(timestamp){
 
+    console.log(`${timestamp}d5c5f7df458e2e0d3207ec1c9f42acce2739bb2fccbad2fc930ed4575d1bb5e4ba6026cc`)
+
     const stringToHash = `${timestamp}d5c5f7df458e2e0d3207ec1c9f42acce2739bb2fccbad2fc930ed4575d1bb5e4ba6026cc`; 
 
     
